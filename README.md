2026d3aec97b6278f3304baf2274122db5fa6c07
Connor McBride <cmcbride1024@gmail.com>
2025-12-16 17:37:47 -0700

    added one hot encoding for each pixel

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bayesian-flow
 Final Project for BYU CS 674
+
+https://arxiv.org/abs/2308.07037
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-import torch 
+import torch
+import torch.nn.functional as F
 
 def to_4bit(x8):
     """
@@ -13,3 +14,11 @@ def to_4bit(x8):
 def normalize_4bit(x4):
     """Normalize 4-bit values ot [0,1]"""
     return x4.float() / 15.0
+
+def one_hot_4bit(x4):
+    """One-hot encode each pixel/channel"""
+    # x4: [B,3,H,W] integer 0..15
+    return F.one_hot(x4, num_classes=16) \
+        .permute(0,1,4,2,3) \
+        .reshape(x4.shape[0], 3*16, x4.shape[2], x4.shape[3]) \
+        .float()
